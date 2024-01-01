fb2c04ffc198051f81c9eff418e8680bd08499a6
zero2080 <zero2080@naver.com>
2024-01-01 22:58:09 +0900

    [추가] 라이센스추가

--- src/notionPageRequest.js
+++ src/notionPageRequest.js
@@ -1,94 +1,96 @@
 class NotionPageRequest {
-    constructor(databaseId, committer_id, job, banch, commit) {
-        let [title, message, foot] = messageParser(commit.message);
-        this.parent = { "database_id": databaseId };
-        this.properties = {
-            "Job": {
-                "select": {
-                    "name": job
-                },
-            },
-            "Type": {
-                "select": {
-                    "name": typeParser(title)
-                }
-            },
-            "Hot-fix": title.indexOf('핫') > 0 ? {
-                "select": {
-                    "name": "O"
-                }
-            } : undefined,
-            "Title": {
-                "title": [
-                    {
-                        "text": {
-                            "content": title
-                        }
-                    }
-                ]
-            },
-            "Committer": committer_id ? {
-                "people": [{
-                    "id": committer_id
-                }]
-            }:undefined,
-            "Deploy": {
-                "select": {
-                    "name": banch
-                }
-            },
-            "DeployedAt": {
-                "date": {
-                    "start": commit.timestamp
-                }
+  constructor(databaseId, committer_id, job, branch, commit) {
+    let [title, message, foot] = messageParser(commit.message);
+    this.parent = {database_id: databaseId};
+    this.properties = {
+      Job: {
+        select: {
+          name: job,
+        },
+      },
+      Type: {
+        select: {
+          name: typeParser(title),
+        },
+      },
+      'Hot-fix':
+        title.indexOf('핫') > 0
+          ? {
+              select: {
+                name: 'O',
+              },
             }
-        };
-        this.children = createChildren(message, foot)
-    }
+          : undefined,
+      Title: {
+        title: [
+          {
+            text: {
+              content: title,
+            },
+          },
+        ],
+      },
+      Committer: {
+        people: [
+          {
+            id: committer_id,
+          },
+        ],
+      },
+      Deploy: {
+        select: {
+          name: branch,
+        },
+      },
+      DeployedAt: {
+        date: {
+          start: commit.timestamp,
+        },
+      },
+    };
+    this.children = createChildren(message, foot);
+  }
 }
 
 function typeParser(title) {
-    return title.substring(0, title.indexOf(']')).replaceAll(/[[핫]/g, '');
+  return title.substring(0, title.indexOf(']')).replaceAll(/[[핫]/g, '');
 }
 
 function messageParser(message) {
-    
-    let body = message
-        .split('\n')
-        .map(msg => msg.trim())
-        .filter(msg => msg.length > 0)
-    let title = body.shift();
-    let foot = body.length > 2 ? body[2].indexOf('](') > 0 ? body.pop() : undefined : undefined;
-    return [title, body, foot];
+  let body = message
+    .split('\n')
+    .map(msg => msg.trim())
+    .filter(msg => msg.length > 0);
+  let title = body.shift();
+  let foot = body.length > 2 ? (body[2].indexOf('](') > 0 ? body.pop() : undefined) : undefined;
+  return [title, body, foot];
 }
 
 function createChildren(message, foot) {
-    let result = message.map(msg => (
+  let result = message.map(msg => ({
+    object: 'block',
+    type: 'paragraph',
+    paragraph: {
+      rich_text: [
         {
-            "object": "block",
-            "type": "paragraph",
-            "paragraph": {
-                "rich_text": [
-                    {
-                        "type": "text",
-                        "text": {
-                            "content": msg
-                        }
-                    }
-                ]
-            }
-        }
-    ));
-    if (foot !== null && foot !== undefined && foot.length > 0) {
-        result.push({
-            "object": "block",
-            "type": "bookmark",
-            "bookmark": {
-                "url": foot.substring(foot.indexOf(']') + 1).replaceAll(/[()]/g, '')
-            }
-        })
-    }
-    return result;
+          type: 'text',
+          text: {
+            content: msg,
+          },
+        },
+      ],
+    },
+  }));
+  if (foot !== null && foot !== undefined && foot.length > 0) {
+    result.push({
+      object: 'block',
+      type: 'bookmark',
+      bookmark: {
+        url: foot.substring(foot.indexOf(']') + 1).replaceAll(/[()]/g, ''),
+      },
+    });
+  }
+  return result;
 }
 
 export default NotionPageRequest;
